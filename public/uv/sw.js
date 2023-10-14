14c77e146931a0dd5a0d473c86710f02bd835e55
Poodleschool <115908009+Poodleschool@users.noreply.github.com>
2023-10-14 06:29:11 -0600

    Update sw.js

--- public/uv/sw.js
+++ public/uv/sw.js
@@ -5,10 +5,11 @@
  * Ideally, this will be registered under the scope in uv.config.js so it will not need to be modified.
  * However, if a user changes the location of uv.bundle.js/uv.config.js or sw.js is not relative to them, they will need to modify this script locally.
  */
-importScripts('uv.bundle.js');
-importScripts('uv.config.js');
-importScripts(__uv$config.sw || 'uv.sw.js');
+importScripts('/anon-shield/public/uv.bundle.js'); // Update this line
+importScripts('/anon-shield/public/uv.config.js'); // Update this line
+importScripts(__uv$config.sw || '/anon-shield/public/uv.sw.js'); // Update this line
 
 const sw = new UVServiceWorker();
 
 self.addEventListener('fetch', (event) => event.respondWith(sw.fetch(event)));
+
