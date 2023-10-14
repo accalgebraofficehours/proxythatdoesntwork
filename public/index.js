9ec30497ffd157a757010bf162e3355200ae4cf5
Poodleschool <115908009+Poodleschool@users.noreply.github.com>
2023-10-14 06:25:08 -0600

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-"use strict";
+use strict";
 /**
  * @type {HTMLFormElement}
  */
@@ -32,5 +32,6 @@ form.addEventListener("submit", async (event) => {
   }
 
   const url = search(address.value, searchEngine.value);
-  location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+  location.href = "/anon-shield/public/uv/service"; // Updated URL path
 });
+
